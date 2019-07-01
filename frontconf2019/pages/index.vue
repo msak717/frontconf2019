9fa81a2d9dd152a08b3775b6318e4fa115d534af
masaki <msak717@gmail.com>
2019-07-01 18:16:04 +0900

    first commit

---
+++ frontconf2019/pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <section class="title">
+        <img src="~/assets/img/kfuglogo.png" width="50" height="50" alt="photo">
+       <h1 class="frontconf_title">FRONTENDCONFERENCE2019</h1>
+    </section>
+    <section class="copy">
+      <h2>ririkoririrkoririkoririkoriirko</h2>
+    </section>
+  
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.container{
+  max-width: 100%;
+}
+
+.title{
+  background-color:#222222;
+  height: 12em;
+  text-align: center;
+  padding: 2em;
+  color:white;
+}
+.title img{
+  margin-bottom: 1.5em; 
+}
+.frontconf_title{
+  margin-bottom: 3em; 
+}
+.copy{
+  text-align: center;
+  margin: 1em;
+  font-family:Arial;
+  font-weight: lighter;
+}
+</style>
